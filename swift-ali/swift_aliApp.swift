c893c47408a4656617ce4d08fea8231474136f2a
Freaks <galleyhugo@icloud.com>
2025-10-02 10:19:10 +0200

    First Commit

---
+++ swift-ali/swift_aliApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_aliApp.swift
+//  swift-ali
+//
+//  Created by Hugo Galley on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_aliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-aliTests/swift_aliTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_aliTests.swift
+//  swift-aliTests
+//
+//  Created by Hugo Galley on 02/10/2025.
+//
+
+import Testing
+@testable import swift_ali
+
+struct swift_aliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
